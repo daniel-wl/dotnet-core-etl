bb19cfdc323fe0124cb0c7279b8872cd462ca7aa
Daniel Long <daniel0long@gmail.com>
2018-05-09 21:51:40 -0400

    Initialize Repo

---
+++ TransactionReportGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TransactionReportGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
