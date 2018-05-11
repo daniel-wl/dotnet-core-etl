a53b231473b02810320d141e4fce4498feceeabd
Daniel Long <daniel0long@gmail.com>
2018-05-10 20:48:36 -0400

    Add parameter checking on main

--- TransactionReportGenerator/Program.cs
+++ TransactionReportGenerator/Program.cs
@@ -9,11 +9,24 @@ namespace TransactionReportGenerator
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            string dataFile = Path.GetFullPath(@"./Data.csv");
-            Console.WriteLine(Directory.GetCurrentDirectory());
-            Console.WriteLine(dataFile);
+            if(args.Length != 1)
+            {
+                Console.WriteLine("Usage: dotnet run PathToCsv");
+                return 1;
+            }
+
+            string dataFile = Path.GetFullPath(args[0]);
+            if(!File.Exists(dataFile))
+            {
+                Console.WriteLine($"File at {dataFile} does not exist.");
+                return 1;
+            }
+            
+            Transaction[] transactions = LoadTransactions(dataFile);
+
+            return 0;
         }
 
         public static Transaction[] LoadTransactions(string pathToCsvFile)
