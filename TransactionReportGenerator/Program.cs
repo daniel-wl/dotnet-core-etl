572dcd35c469cbb79d3cac6dd48924774c08a173
Daniel Long <daniel0long@gmail.com>
2018-05-10 21:35:25 -0400

    Implement Menu

--- TransactionReportGenerator/Program.cs
+++ TransactionReportGenerator/Program.cs
@@ -17,14 +17,14 @@ namespace TransactionReportGenerator
                 return 1;
             }
 
-            string dataFile = Path.GetFullPath(args[0]);
-            if(!File.Exists(dataFile))
+            string csvFile = Path.GetFullPath(args[0]);
+            if(!File.Exists(csvFile))
             {
-                Console.WriteLine($"File at {dataFile} does not exist.");
+                Console.WriteLine($"File at {csvFile} does not exist.");
                 return 1;
             }
-            
-            Transaction[] transactions = LoadTransactions(dataFile);
+
+            RunMenuLoop(csvFile);
 
             return 0;
         }
@@ -34,6 +34,69 @@ namespace TransactionReportGenerator
             CsvReader csvReader = new CsvReader(File.OpenText(pathToCsvFile));
             csvReader.Configuration.RegisterClassMap<TransactionMap>();
             return csvReader.GetRecords<Transaction>().ToArray();
+        }
+
+        public static void RunMenuLoop(string csvFile)
+        {
+            ConsoleKey choice;            
+            PrintTitle();
+
+            do
+            {
+                PrintMenu();
+                choice = GetInput();
+                RunBusinessLogic(choice, csvFile);
+            }
+            while(choice != ConsoleKey.D0);
+        }
+
+        public static void PrintTitle()
+        {
+            Console.WriteLine("************************************");
+            Console.WriteLine("*** Transaction Report Generator ***");
+            Console.WriteLine("************************************");
+        }
+
+        public static void PrintMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("************ Main  Menu ************");
+            Console.WriteLine("(1) Sales Summary");
+            Console.WriteLine("(2) Assets Under Management Summary");
+            Console.WriteLine("(3) Break Report");
+            Console.WriteLine("(4) Investor Profit Report");
+            Console.WriteLine("(0) Quit");
+            Console.WriteLine();
+        }
+
+        public static ConsoleKey GetInput()
+        {
+            Console.Write("Please enter a number corresponding to your menu choice: ");
+            ConsoleKey input =  Console.ReadKey().Key;
+            Console.WriteLine();
+            return input;
+        }
+
+        public static void RunBusinessLogic(ConsoleKey choice, string csvFile)
+        {
+            switch(choice)
+            {
+                case ConsoleKey.D1:
+                    Transaction[] transactions = LoadTransactions(csvFile);    
+                    break;
+                case ConsoleKey.D2:
+                    break;
+                case ConsoleKey.D3:
+                    break;
+                case ConsoleKey.D4:
+                    break;
+                case ConsoleKey.D0:
+                    Console.WriteLine("Goodbye.");
+                    break;
+                default:
+                    Console.WriteLine("Invalid selection. Please try again.");
+                    break;
+            }
         }
     }
 }
