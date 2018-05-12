85b697ff3412192dd3449e58f5d426c1cbe26daf
Daniel Long <daniel0long@gmail.com>
2018-05-11 22:48:48 -0400

    Sales Summary core finished

--- TransactionReportGenerator.Test/SalesSummaryTest.cs
+++ TransactionReportGenerator.Test/SalesSummaryTest.cs
@@ -3,38 +3,105 @@ using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using TransactionReportGenerator.Models;
+using System.Linq;
 
 namespace TransactionReportGenerator.Test
 {
     public class SalesSummaryTest
     {
         [Test]
-        public void CreateSalesSummaryWithNullTransactions()
+        public void CreateSalesSummaryWithNullTransactionsTest()
         {
             Assert.Throws<ArgumentNullException>(() => new SalesSummary(null), "Creating a sales summary with null transactions should throw");
         }
 
         [Test]
-        public void CreateSalesSummaryWithEmptyTransactions()
+        public void CreateSalesSummaryWithEmptyTransactionsTest()
         {
             Assert.Throws<ArgumentException>(() => new SalesSummary(new List<Transaction>()), "Creating a sales summary with empty transactions should throw");
         }
 
         [Test]
-        public void CreateSalesSummaryWithTransactions()
+        public void CreateSalesSummaryWithTransactionsTest()
         {
             Assert.DoesNotThrow(() => new SalesSummary(new List<Transaction>(GetFakeTransactions())), "Creating a sales summary with a non-empty transaction list should not throw.");
         }
 
-        
-
-        private IEnumerable<Transaction> GetFakeTransactions()
+        [Test]
+        public void GetUniqueInvestorsTest()
         {
-            yield return new Transaction();
-            yield return new Transaction();
-            yield return new Transaction();
-            yield return new Transaction();
-            yield return new Transaction();
+            string[] investors = SalesSummary.GetUniqueInvestors(GetFakeTransactions());
+            Assert.AreEqual(investors.Length, 3, "Incorrect number of unique investors");
+            Assert.IsNotNull(investors.SingleOrDefault(i => i == "John Doe"), "Investor John Doe not found.");
+            Assert.IsNotNull(investors.SingleOrDefault(i => i == "Samantha Sample"), "Investor Samantha Sample not found.");
+            Assert.IsNotNull(investors.SingleOrDefault(i => i == "Tom Test"), "Investor Tom Test not found.");
+        }
+
+        [Test]
+        public void GetSellAmountsPerInvestorTestTest()
+        {
+            double[] sellAmounts = SalesSummary.GetSellAmountsForInvestor("John Doe", GetFakeTransactions());
+            Assert.AreEqual(2, sellAmounts.Length, "Incorrect number of sale prices");
+            Assert.IsNotNull(sellAmounts.SingleOrDefault(i => i == 1), "Expected sale price not found.");
+            Assert.IsNotNull(sellAmounts.SingleOrDefault(i => i == 2), "Expected sale price not found.");
+            Assert.Throws<InvalidOperationException>(() => sellAmounts.Single(i => i == 3), "Price for buy transaction should not be included.");
+        }
+
+        [Test]
+        public void GetSummaryForDateRangeTest()
+        {
+            SalesSummary salesSummary = new SalesSummary(GetFakeTransactions());
+            var totalSold = salesSummary.GetTotalSoldForDateRangePerInvestor(DateTime.Now.Subtract(new TimeSpan(2, 0, 0, 0)));
+            Assert.IsNotNull(totalSold, "Sales summary should not be null");
+            Assert.IsNotEmpty(totalSold, "Sales summary should not be empty.");
+        }
+
+        [Test]
+        public void FilterTransactionsByDateTest()
+        {
+            SalesSummary salesSummary = new SalesSummary(GetFakeTransactions());
+        }
+
+        private List<Transaction> GetFakeTransactions()
+        {
+            return new List<Transaction>
+            {
+                new Transaction
+                {
+                    Investor = "John Doe",
+                    Price = 1,
+                    TransactionType = TransactionType.Sell,
+                    Date = DateTime.Now.Subtract(new TimeSpan(days: 1, hours: 0, minutes:0, seconds: 0))
+                },
+                new Transaction
+                {
+                    Investor = "Samantha Sample",
+                    Price = 1,
+                    TransactionType = TransactionType.Sell,
+                    Date = DateTime.Now.Subtract(new TimeSpan(days: 1, hours: 0, minutes:0, seconds: 0))                    
+                },
+                new Transaction
+                {
+                    Investor = "John Doe",
+                    Price = 2,
+                    TransactionType = TransactionType.Sell,
+                    Date = DateTime.Now.Subtract(new TimeSpan(days: 2, hours: 0, minutes:0, seconds: 0))                    
+                },
+                new Transaction
+                {
+                    Investor = "John Doe",
+                    Price = 3,
+                    TransactionType = TransactionType.Buy,
+                    Date = DateTime.Now.Subtract(new TimeSpan(days: 3, hours: 0, minutes:0, seconds: 0))                    
+                },
+                new Transaction
+                {
+                    Investor = "Tom Test",
+                    Price = 1,
+                    TransactionType = TransactionType.Sell,
+                    Date = DateTime.Now.Subtract(new TimeSpan(days: 1, hours: 1, minutes:0, seconds: 0))                    
+                }
+            };
         }
     }
 }
--- TransactionReportGenerator/BusinessLogic/SalesSummary.cs
+++ TransactionReportGenerator/BusinessLogic/SalesSummary.cs
@@ -24,14 +24,34 @@ namespace TransactionReportGenerator.BusinessLogic
             Transactions = transactions;
         }
 
-        public void GetInceptionToDateSummary()
+        internal Dictionary<string, double> GetTotalSoldForDateRangePerInvestor(DateTime startDate)
         {
-            string[] investors = Transactions.Select(t => t.Investor).Distinct().ToArray();
+            List<Transaction> filteredTransactions = FilterTransactionsByDate(startDate);
+            string[] investors = GetUniqueInvestors(filteredTransactions);
+
             Dictionary<string, double> totalSoldPerInvestor = new Dictionary<string, double>();
             foreach(string investor in investors)
             {
-                Transactions.Where(t => t.TransactionType == TransactionType.Sell && t.Investor == investor).Select(t => t.Price);
+                double totalSold = GetSellAmountsForInvestor(investor, filteredTransactions).Sum();
+                totalSoldPerInvestor.Add(investor, totalSold);
             }
+
+            return totalSoldPerInvestor;
+        }
+
+        internal static string[] GetUniqueInvestors(List<Transaction> transactions)
+        {
+            return transactions.Select(t => t.Investor).Distinct().ToArray();
+        }
+
+        internal static double[] GetSellAmountsForInvestor(string investor, List<Transaction> transactions)
+        {
+            return transactions.Where(t => t.Investor == investor && t.TransactionType == TransactionType.Sell).Select(t => t.Price).ToArray();
+        }
+
+        internal List<Transaction> FilterTransactionsByDate(DateTime startDate)
+        {
+            return Transactions.Where(t => t.Date >= startDate).ToList();
         }
     }
 }
