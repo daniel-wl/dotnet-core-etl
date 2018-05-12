230c87af92efe6b967ce1b2f926ae34745bf7cab
Daniel Long <daniel0long@gmail.com>
2018-05-11 20:20:07 -0400

    Add extra test for sales summary parameter check

--- TransactionReportGenerator.Test/SalesSummaryTest.cs
+++ TransactionReportGenerator.Test/SalesSummaryTest.cs
@@ -19,5 +19,22 @@ namespace TransactionReportGenerator.Test
         {
             Assert.Throws<ArgumentException>(() => new SalesSummary(new List<Transaction>()), "Creating a sales summary with empty transactions should throw");
         }
+
+        [Test]
+        public void CreateSalesSummaryWithTransactions()
+        {
+            Assert.DoesNotThrow(() => new SalesSummary(new List<Transaction>(GetFakeTransactions())), "Creating a sales summary with a non-empty transaction list should not throw.");
+        }
+
+        
+
+        private IEnumerable<Transaction> GetFakeTransactions()
+        {
+            yield return new Transaction();
+            yield return new Transaction();
+            yield return new Transaction();
+            yield return new Transaction();
+            yield return new Transaction();
+        }
     }
 }
